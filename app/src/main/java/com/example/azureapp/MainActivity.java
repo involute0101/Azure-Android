75fc18020448e5ea0403f7de0206b9cfe4a77a5d
wzk <1403235458@qq.com>
2021-07-19 15:26:18 +0800

    警告通知，个人主页

--- app/src/main/java/com/example/azureapp/MainActivity.java
+++ app/src/main/java/com/example/azureapp/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.azureapp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,10 +24,23 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.azureapp.databinding.ActivityMainBinding;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    NotificationManager manager;
+    NotificationChannel channel;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +58,49 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
 
 
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        channel = new NotificationChannel("1", "不知道这个是啥", NotificationManager.IMPORTANCE_DEFAULT);
+        manager.createNotificationChannel(channel);
+        sendAlerts();
+
+
+
+
     }
+
+    private void sendAlerts() {
+
+        new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        String channelId = "007";
+                        String channelName = "James Bond";
+                        NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+                        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                        notificationManager.createNotificationChannel(notificationChannel);
+                        Notification notification = new NotificationCompat.Builder(MainActivity.this, "007")
+                                .setContentTitle("警告")
+                                .setContentText("服务出现问题")
+                                .setWhen(System.currentTimeMillis())
+                                .setSmallIcon(R.drawable.icon_alert)
+                                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon_alert))
+                                .setAutoCancel(true)
+
+                                .build();
+                        notificationManager.notify(1, notification);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
+    }
+
 
 }
--- app/src/main/java/com/example/azureapp/ui/user/UserFragment.java
+++ app/src/main/java/com/example/azureapp/ui/user/UserFragment.java
@@ -9,20 +9,35 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.azureapp.LoginActivity;
 import com.example.azureapp.R;
+import com.example.azureapp.StartActivity;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 public class UserFragment extends Fragment {
 
     private UserViewModel mViewModel;
     Button quitButton;
-    TextView userEmailTV,userInfoTV;
+    TextView userEmailTV, userTypeTV;
+    public String userEmail;
+    public String userType;
 
     public static UserFragment newInstance() {
         return new UserFragment();
@@ -41,18 +56,65 @@ public class UserFragment extends Fragment {
         // TODO: Use the ViewModel
         quitButton = requireActivity().findViewById(R.id.quit_button);
         userEmailTV = requireActivity().findViewById(R.id.user_email_tv);
-        userInfoTV = requireActivity().findViewById(R.id.user_info_tv);
+        userTypeTV = requireActivity().findViewById(R.id.user_type_tv);
+        getUserInfo();
+        userEmailTV.setText(userEmail);
+        if (userType.equals("user")){userTypeTV.setText("用户");}
 
-        /*userEmailTV.setText("");
-        userInfoTV.setText("");*/
+
+
 
         quitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), LoginActivity.class);
+                Intent intent = new Intent(getContext(), StartActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    /**
+     * 获取用户信息
+     */
+    private void getUserInfo() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String url = "http://20.89.169.250:8080/Azure/getAccountInfo";
+                HttpClient client = HttpClients.createDefault();
+                HttpGet get = new HttpGet(url);
+                try{
+                    HttpResponse response = client.execute(get);
+                    int statusCode = response.getStatusLine().getStatusCode();
+                    if (statusCode == 200) {
+                        String result = EntityUtils.toString(response.getEntity());
+                        JSONObject object = (JSONObject) JSONObject.parse(result);
+                        JSONObject user = object.getJSONObject("user");
+                        userEmail = user.getString("name");
+                        userType = user.getString("type");
+
+
+                            Log.d("user",userEmail);
+                            //获取到数据库的描述信息
+
+                        //System.out.println("結果："+result);
+                        //vmAdapter.notifyDataSetChanged();
+
+                    }
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            thread.start();
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
